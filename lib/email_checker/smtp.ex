bc69ed7fd025ffc72e6f1fa1f93ecfc1818cfb52
Kevin Disneur <kevin@disneur.me>
2015-11-22 20:33:10 +0100

    Fix errors when SMTP doesn't reply or Socket is closed

--- lib/email_checker/smtp.ex
+++ lib/email_checker/smtp.ex
@@ -1,5 +1,32 @@
 defmodule EmailChecker.SMTP do
   def valid?(email) do
+    valid?(email, 2)
+  end
+
+  defp valid?(email, retries) when retries == 0 do
+    false
+  end
+  defp valid?(email, retries) do
+    try do
+      case smtp_reply(email) do
+        nil ->
+          false
+        response ->
+          Regex.match?(~r/^250 /, response)
+      end
+    rescue
+      Socket.Error ->
+        valid?(email, retries - 1)
+    end
+  end
+
+  defp mx_address(email) do
+    email
+    |> EmailChecker.Tools.domain_name
+    |> EmailChecker.MX.lookup
+  end
+
+  def smtp_reply(email) do
     socket =
       email
       |> mx_address
@@ -13,12 +40,6 @@ defmodule EmailChecker.SMTP do
     socket |> Socket.Stream.recv!
 
     socket |> Socket.Stream.send!("rcpt to:<#{email}>\r\n")
-    Regex.match?(~r/^250 /, socket |> Socket.Stream.recv!)
-  end
-
-  defp mx_address(email) do
-    email
-    |> EmailChecker.Tools.domain_name
-    |> EmailChecker.MX.lookup
+    socket |> Socket.Stream.recv!
   end
 end
