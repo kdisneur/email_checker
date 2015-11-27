4ba851a83073564a7e3b1cdc0a8eaacdc9ade12e
Armando Di Cianno <armando@dicianno.org>
2015-11-27 13:43:01 -0500

    Add support for timeouts (and tests); add configurable smtp retries and timeouts; change default DNS server to more obviously public one
    # config.exs
    ## Configure options  for email_checker
     * default_dns -- set to Google's primary public DNS server 8.8.8.8, and documented as such.
     * smtp_retries - 2 still default, but now configurable
     * timeout_milliseconds -- timeout to use for MX and SMTP validation strategies (so, coupled with smtp_retries, this isn't about how much latency may occur overall in single call to `valid?`, but how long each strategy has to use internet activities that may take very long)
    
    # loader.ex
    Use new default Google's public DNS server
    ## inet_db.add_ns
    I was curious about this too, and looked into it. Painfully, looks like the generated docs/man-page for inet_db was removed in R18. I felt it was good to match or explode on the result being as expected (:ok), since missing this is effectively a program error -- i.e. if the default_dns is configured, this call had better succeed
    
    # mx.ex
    Use timeout_miliseconds.
    
    # smtp.ex
    Use timeout_miliseconds, and spread that total over the entire validation.
    
    # timeout_test.exs
    * 2 tests for a known good email and server, with :infinity and 5000 ms timeouts
    * 1 test for a known bad email/server with a 5000 ms timeout to assert it it ending sooner than :infinity
    
    # README.md
    Documentation related to the new timeout and retry configuration options

---
+++ test/email_checker/timeout_test.exs
@@ -0,0 +1,67 @@
+defmodule EmailChecker.TimeoutTest do
+  use ExUnit.Case, async: false
+
+  # start/stop timer strategy ported from Erlang lib/kernel/src/inet.erl
+  @spec stop_timer(nil) :: true
+  @spec stop_timer(reference) :: boolean
+  @spec start_timer(:infinity) :: nil
+  @spec start_timer(non_neg_integer) :: reference
+
+  defp start_timer(:infinity), do: nil
+  defp start_timer(timeout), do: :erlang.start_timer(timeout, self, :tooslow)
+
+  defp stop_timer(nil), do: true
+  defp stop_timer(timer) do
+    case :erlang.cancel_timer(timer) do
+      false ->
+        # false, if the timer was unable to be canceled
+        receive do
+          {:timeout,_timer,_msg} -> false
+  	    after
+          0 -> false
+  	    end
+      _ ->
+        # true, if the timer was able to be canceled
+        true
+    end
+  end
+
+  test "timeout: :infinity works for known good email address and servers" do
+    timeout = :infinity
+    Application.put_env(:email_checker, :timeout_milliseconds, timeout)
+    Application.put_env(:email_checker, :smtp_retries, 1)
+
+    timer = start_timer(timeout)
+    result = EmailChecker.valid? "kevin@disneur.me"
+    timer_not_fired = stop_timer(timer)
+
+    assert timer_not_fired
+    assert result
+  end
+
+  test "timeout: non_neg_integer works for known good email address and servers" do
+    timeout = 5000
+    Application.put_env(:email_checker, :timeout_milliseconds, timeout)
+    Application.put_env(:email_checker, :smtp_retries, 1)
+
+    timer = start_timer(timeout)
+    result = EmailChecker.valid? "kevin@disneur.me"
+    timer_not_fired = stop_timer(timer)
+
+    assert timer_not_fired
+    assert result
+  end
+
+  test "timeout: non_neg_integer works to quickly return from checking a known bad email address or server" do
+    timeout = 5000
+    Application.put_env(:email_checker, :timeout_milliseconds, timeout)
+    Application.put_env(:email_checker, :smtp_retries, 1)
+
+    timer = start_timer(timeout)
+    _ = EmailChecker.valid? "derp@asdf.com"
+    timer_not_fired = stop_timer(timer)
+
+    assert !timer_not_fired
+  end
+
+end
